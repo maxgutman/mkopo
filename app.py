5f174ea4164245414f244add24c892714e0d16d6
maxgutman <mxgutman@gmail.com>
2013-10-19 00:34:15 -0700

    updates

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, redirect, url_for, session, request
 from flask import render_template
 from flask_oauth import OAuth
-
+from models import User
 
 SECRET_KEY = '123'
 DEBUG = True
@@ -24,15 +24,39 @@ facebook = oauth.remote_app('facebook',
     request_token_params={'scope': 'email'}
 )
 
-@app.route("/")
-def index():
+def get_context():
     data = session.get('data')
-    context = {}
     if data:
-        context.update({
+        return {
+            'email': data['email'],
             'name': data['name'],
             'photo_url': 'https://graph.facebook.com/%s/picture' % data['id'],
-        })
+        }
+    return {}
+
+
+@app.route("/loan", methods=['POST', 'GET'])
+def loan_form():
+    context = get_context()
+    print request.form
+    if request.method == 'POST':
+        loan_amount = request.form['loan_amount']
+        user = User.get_or_create(email=context['email'])
+        user.loan_amount = loan_amount
+        user.save()
+        return redirect(url_for('index'))
+    return render_template('loan_form.html', **context)
+
+@app.route("/")
+def index():
+    context = get_context()
+    if context:
+        user = User.get_or_create(email=context['email'])
+        if not user.loan_amount:
+            return redirect(url_for('loan_form'))
+
+        all_users = User.get_all(exclude_user_id=user.id)
+        context.update({'all_users': list(all_users)})
     return render_template('index.html', **context)
 
 @facebook.tokengetter
@@ -53,12 +77,20 @@ def facebook_login():
 @facebook.authorized_handler
 def facebook_authorized(resp):
     next_url = request.args.get('next') or url_for('index')
+    print 'here', next_url
     if resp is None or 'access_token' not in resp:
         return redirect(next_url)
 
     session['logged_in'] = True
     session['facebook_token'] = (resp['access_token'], '')
-    session['data'] = facebook.get('/me').data
+    data = facebook.get('/me').data
+    session['data'] = data
+    User.get_or_create(
+        email=data['email'],
+        name=data['name'],
+        facebook_id=data['id'],
+    )
+
     return redirect(next_url)
 
 @app.route("/logout")
---
+++ models.py
@@ -0,0 +1,121 @@
+import datetime
+
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import (
+    relation,
+    backref,
+    mapper,
+    sessionmaker,
+    scoped_session,
+)
+
+engine = create_engine('sqlite:////tmp/test.db', convert_unicode=True)
+db = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+Base = declarative_base()
+Base.query = db.query_property()
+
+class CRUDMixin(object):
+    __table_args__ = {'extend_existing': True}
+
+    id = Column(Integer, primary_key=True)
+
+    @classmethod
+    def get_by_id(cls, id):
+        if any(
+            (isinstance(id, basestring) and id.isdigit(),
+             isinstance(id, (int, float))),
+        ):
+            return cls.query.get(int(id))
+        return None
+
+    @classmethod
+    def get_or_create(cls, **kwargs):
+        instance = db.query(cls).filter_by(**kwargs).first()
+        if not instance:
+            instance = cls(**kwargs)
+            instance.save()
+        return instance
+
+    @classmethod
+    def create(cls, **kwargs):
+        instance = cls(**kwargs)
+        return instance.save()
+
+    def update(self, commit=True, **kwargs):
+        for attr, value in kwargs.iteritems():
+            setattr(self, attr, value)
+        return commit and self.save() or self
+
+    def save(self, commit=True):
+        db.add(self)
+        if commit:
+            db.commit()
+        return self
+
+    def delete(self, commit=True):
+        db.delete(self)
+        return commit and db.commit()
+
+
+user_group = Table('user_group', Base.metadata,
+    Column('id', Integer(), primary_key=True, nullable=False),
+    Column('user_id', Integer(), ForeignKey('user.id'), nullable=False),
+    Column('group_id', Integer(), ForeignKey('group.id'), nullable=False),
+    Column('created', DateTime(), default=datetime.datetime.now),
+)
+
+
+class UserGroup(CRUDMixin, Base):
+    __tablename__ = 'user_group'
+    __table__ = user_group
+
+    group = relation('Group', primaryjoin='UserGroup.group_id==Group.id')
+    user = relation('User', primaryjoin='UserGroup.user_id==User.id')
+
+
+class User(CRUDMixin, Base):
+    __tablename__ = 'user'
+
+    email = Column(String(255), unique=True)
+    name = Column(String(255))
+    loan_amount = Column(String(255))
+    facebook_id = Column(BigInteger(20, unsigned=True))
+    groups = relation('Group', primaryjoin='User.id==UserGroup.user_id', secondary=UserGroup.__table__, secondaryjoin='UserGroup.group_id==Group.id')
+
+    def __init__(self, email, name=None, loan_amount=None, facebook_id=None):
+        self.email = email
+        self.name = name
+        self.loan_amount = loan_amount
+        self.facebook_id = facebook_id
+
+    def __repr__(self):
+        return u'<User %r>' % self.email
+
+    @classmethod
+    def get_or_create(cls, **kwargs):
+        instance = db.query(cls).filter_by(email=kwargs.get('email')).first()
+        if not instance:
+            instance = User.create(**kwargs)
+        return instance
+
+    @classmethod
+    def get_all(cls, **kwargs):
+        return db.query(cls).filter(User.id != kwargs.get('exclude_user_id'))
+
+
+class Group(CRUDMixin, Base):
+    __tablename__ = 'group'
+
+    name = Column(String(255))
+    domain = Column(String(255))
+
+    users = relation('User', primaryjoin='Group.id==UserGroup.group_id', secondary=UserGroup.__table__, secondaryjoin='UserGroup.user_id==User.id')
+
+    def __repr__(self):
+        return u'<Group %r>' % self.name
+
+
+Base.metadata.create_all(bind=engine)
